cff3f93c48213aef909af3dc2e16163bc3ceecaa
Kzian <anwarcassia@gmail.com>
2025-08-08 17:57:11 +0100

    Add Heroku deployment workflow

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-const assert = require('assert');
+const request = require('supertest');
+const app = require('../index'); // make sure index.js exports the app
 
-describe('Basic Test', () => {
-  it('should return true', () => {
-    assert.strictEqual(true, true);
+describe('GET /', () => {
+  it('should return Hello World!', async () => {
+    const res = await request(app).get('/');
+    if (res.statusCode !== 200) throw new Error('Status not 200');
+    if (res.text !== 'Hello World!') throw new Error('Unexpected response body');
   });
 });
-
-
